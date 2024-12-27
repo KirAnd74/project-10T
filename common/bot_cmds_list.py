c0854d16e3fa2dcf7a14799934d205b123c55896
Кирилл Горчаков <kirill.gorchakov08@gmail.com>
2024-12-27 22:01:49 +0300

    0.3

---
+++ common/bot_cmds_list.py
@@ -0,0 +1,6 @@
+from aiogram.types import BotCommand
+private = [
+    BotCommand(command = "/start", description = "Начать работу с ботом"),
+    BotCommand(command = "/menu", description = "Показать меню"),
+    BotCommand(command = "/help", description = "Помощь"),
+]
